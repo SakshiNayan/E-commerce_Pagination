e831a992794d30c7008934386845586db9801371
Sakshi Nayan <sakshi12princi@gmail.com>
2022-10-17 14:02:10 +0530

    popUp doing

--- src/Component/productPg.jsx
+++ src/Component/productPg.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import axios from 'axios'
 import './productPg.css'
 import Pagination from './pagination';
+import Popup from './popup';
 function ProductPg() {
     const showperPg  =5;
     const [pagination, setpagination] = useState({
@@ -15,7 +16,8 @@ function ProductPg() {
 
     const [alldata, setAlldata]= useState([])
     const [data, setdata]= useState([])
-    // const [show,setshow]=useState()
+    const [show, setShow] = useState(false);
+    const [tempData, settempData] = useState([]);
     const [get , setget] = useState(true)
     
     const handleSelect=(e)=>{
@@ -39,6 +41,21 @@ function ProductPg() {
             setAlldata(fakedata.data)
         })
     },[get])
+
+    const handleModal = (desc, cat, image) => {
+        let tempData = [desc, cat, image];
+        settempData(item => [1, ...tempData]);
+        setShow(true);
+    }
+
+    const handleHoverModal = (desc, cat, image) => {
+        let tempData = [desc, cat, image];
+        settempData(item => [1, ...tempData]);
+        setTimeout(() => {
+            setShow(true)
+        }, 500)
+    }
+
   return (
     <>
         <div id='head'><h1>AVAILABLE PRODUCTS</h1></div>
@@ -46,17 +63,19 @@ function ProductPg() {
             <select onChange={(e)=>{handleSelect(e)}}>
                 <option value='all'>All</option>
                 <option value='electronic'>Electronics</option>
-                <option value='Jwellery'>Jwellery</option>
                 <option value='women'>Women's Cloths</option>
                 <option value='men'>Men's Cloths</option>
+                <option value='Jwellery'>Jwellery</option>
             </select>
         </div>
 
         <div id='main-body'>
             {data.slice(pagination.start , pagination.end).map((item,i)=>{
                 return(
-                    <div key={i}  className="productsection">
-                        <img src={item.image} />
+                    <div key={i}  className="productsection" >
+                        <img src={item.image} 
+                            onClick={() => handleModal(item.description, item.category, item.image)}
+                            onMouseOver={() => handleHoverModal(item.description, item.category, item.image)} />
                     </div>
                 )
             })}
@@ -65,7 +84,10 @@ function ProductPg() {
         <div>
             <Pagination  showperPg={showperPg}  total ={data.length} onPaginationChange = {onPaginationChange}/>
         </div>
-      
+        {
+                show === true ? <Popup image={tempData[3]} desc={tempData[1]} category={tempData[2]} show={show} setShow={setShow} /> : ""
+            }
+        {/* {itempop && <Popup closepop ={setitemPop}/>} */}
     </>
   )
 }
